a44552df4d256bb2446ca2d453e532b920d414de
20025819 <20025819@students.koi.edu.au>
2025-08-10 16:56:40 +0545

    Added a new file name hello in th eproject

---
+++ UpdateSydenyCoffee/hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateSydenyCoffee
+{
+    class hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Invalid Input! Coffee bags between 1 and 200 can be ordered.");
+        }
+    }
+}
